d575c49f8e89d32832a786feb8e613f3021ca8b4
Kate Burson <burson.kate@gmail.com>
2019-06-25 21:34:53 -0700

    db config

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mLabURI: ""
+  mongoURI: "mongodb://<dbuser>:<dbpassword>@ds243717.mlab.com:43717/heroku_29xjg4c9"
 };
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
@@ -10,12 +11,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// const db = require("./config/keys").mLabURI;
+const db = require("./config/keys").mongoURI;
 
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => console.log("MongoDB successfully connected"))
-//   .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
